0f98f96ad94d7f30a6c21c84f8e98c205ca1b0f9
Rafael Metal Omega <62553030+MetalOmega@users.noreply.github.com>
2023-05-03 00:00:04 -0300

    Primeiro código em Flutter

---
+++ hello.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+			  title: Text('First App'),
+		  ),
+      body: Center(
+        child: Text('Hello World'),
+        ),
+    )
+  ));
+}
